9f8d8d30bdcc23f2f250c1a1b9eedc4453661119
edgardoburgos94 <32230637+edgardoburgos94@users.noreply.github.com>
2019-02-06 18:36:27 -0500

    okk

--- solution.rb
+++ solution.rb
@@ -4,6 +4,6 @@ get '/' do
   unless params[:nombre].nil? && params[:nombre].empty?
     erb "<h1>Hola desconocido!</h1>"
   else
-    erb "<h1>Hola #{params[:nombre].constantize}!</h1>"
+    erb "<h1>Hola #{params[:nombre].capitalize}!</h1>"
   end
 end
