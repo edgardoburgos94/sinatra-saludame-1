f4e665e766ed5a887bef080d4a5bc48b033268a4
edgardoburgos94 <32230637+edgardoburgos94@users.noreply.github.com>
2019-02-06 18:41:06 -0500

    .

--- solution.rb
+++ solution.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 
 get '/' do
-  unless params[:nombre].nil? || params[:nombre].empty?
+  unless params[:nombre].nil? || params[:nombre] = ""
     erb "<h1>Hola desconocido!</h1>"
   else
     erb "<h1>Hola #{params[:nombre].capitalize}!</h1>"
