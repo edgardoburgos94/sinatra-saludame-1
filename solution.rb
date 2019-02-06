05626b922f7e639087b8d8e291770327f56c335f
edgardoburgos94 <32230637+edgardoburgos94@users.noreply.github.com>
2019-02-06 18:47:17 -0500

    esta sí pues

--- solution.rb
+++ solution.rb
@@ -2,8 +2,8 @@ require 'sinatra'
 
 get '/' do
   unless params[:nombre].nil? || params[:nombre] == ''
-    erb "<h1>Hola desconocido!</h1>"
-  else
     erb "<h1>Hola #{params[:nombre].capitalize}!</h1>"
+  else
+    erb "<h1>Hola desconocido!</h1>"
   end
 end
